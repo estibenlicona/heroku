0d72c735d8ab238df5e80d94822308afea1477e3
estibenlicona <estibenlicona@gmail.com>
2018-11-28 19:40:11 -0500

    v3

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
