04191854a33f371e81a5a77903fd2c3072c8473a
estibenlicona <estibenlicona@gmail.com>
2018-11-28 19:14:42 -0500

    v1

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/hola', (req, res) => {
+  res.send({message: 'Esto es un Serivicio'});
+})
+app.listen(3000, ()=> {
+  console.log("Hola Mundo Express Prueba");
+})
